ec0fb66914bf39603065f6273479d24774c93faf
venubabu <velpurivenubabu123@gmail.com>
2023-05-03 07:32:01 +0530

    [Venubabu] Done with Triangular Pattern

---
+++ Day  11  and 12  Practice Problems/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Day__11__and_12__Practice_Problems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Welcome to Practice Problems!");
+            Console.WriteLine("Please Select any below Option");
+            Console.WriteLine("1.InvertedRectangularPattern");
+            int Option=Convert.ToInt32(Console.ReadLine());
+            switch(Option)
+            {
+                
+                case 1:
+                    Console.WriteLine("Please enter number of rows you want to print");
+                    int number=Convert.ToInt32(Console.ReadLine());
+                    TringlePattern.GetPattern(number);
+                    break;
+            }
+        }
+    }
+}
---
+++ Day  11  and 12  Practice Problems/TringlePattern.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day__11__and_12__Practice_Problems
+{
+    internal class TringlePattern
+    {
+        public static void GetPattern(int number)
+        {
+            for (int i = 0; i < number; i++)
+            {
+                string pattern = new string('*', (number - i));
+                Console.WriteLine(pattern);
+            }   
+        }
+    }
+}
